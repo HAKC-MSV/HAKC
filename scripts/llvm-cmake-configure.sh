eebb8d5c8a13dc17e147d1da1a11ace751be6d2e
Burow <na28772@578796-mitll.llan.ll.mit.edu>
2023-02-23 10:41:46 -0500

    llvm cmake file

---
+++ scripts/llvm-cmake-configure.sh
@@ -0,0 +1,12 @@
+cmake -G Ninja \
+  -DLLVM_ENABLE_PROJECTS='clang;lld;clang-tools-extra;llvm' \
+  -DLLVM_ENABLE_RUNTIMES=compiler-rt \
+  -DCMAKE_INSTALL_PREFIX=/home/na28772/Code/install \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_C_COMPILER=/usr/bin/clang \
+  -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
+  -DLLVM_TARGETS_TO_BUILD='X86;AArch64' \
+  -DLLVM_OPTIMIZED_TABLEGEN=yes \
+  -DLLVM_USE_LINKER=lld \
+  -DLLVM_ENABLE_IDE=True \
+  ../llvm
