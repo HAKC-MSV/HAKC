d2319fd8fa361160e61d678d63811b385555063e
Derrick McKee <derrick.mckee@gmail.com>
2023-11-13 14:11:52 -0500

    refactoring tests to use correct paths and new pass behavior

--- HAKC-pass/tests/PHINodeTests/test0/build.sh
+++ HAKC-pass/tests/PHINodeTests/test0/build.sh
@@ -3,7 +3,7 @@
 mkdir -p build
 
 env HAKC_ANALYSIS=compartmentalize HAKC_COMPARTMENT_PATH=phi.yml \
-/home/de29664/code/llvm-project/cmake-build-12.x/install-12.x/bin/clang \
+$TEST_CLANG \
   -fexperimental-new-pass-manager \
-  -fpass-plugin=/home/de29664/code/ARM-MTE/cmake-build-x86-hakc-clang-12/HAKC-Compartmentalizer/lib/libHAKC-Compartmentalizer.so \
-  -g -S -emit-llvm -O2 -o build/phi.bc -c phi.c
+  -fpass-plugin=$TEST_PASS \
+  -g -S -emit-llvm -O2 -o build/test.bc -c test.c
