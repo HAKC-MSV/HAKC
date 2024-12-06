b76c66f0e95e34f392c86810e3321d3e00fe6ea3
Allan Nesathurai <Allan.Nesathurai@ll.mit.edu>
2024-12-06 20:57:20 +0000

    update build.sh command

--- HAKC-pass/tests/General/test_0/build.sh
+++ HAKC-pass/tests/General/test_0/build.sh
@@ -1,9 +1,8 @@
 #/bin/sh
-
+export ROOT=$(git rev-parse --show-toplevel)
+export TEST_PASS=$ROOT/install/lib/HAKC-Compartmentalizer.so
 mkdir -p build
 
-env HAKC_ANALYSIS=compartmentalize HAKC_COMPARTMENT_PATH=test-0.yml \
-  $TEST_CLANG \
-  -fexperimental-new-pass-manager \
-  -fpass-plugin=$TEST_PASS \
+$ROOT/install/bin/clang \
+  -Xclang -load -Xclang $TEST_PASS -fpass-plugin=$TEST_PASS \
   -g -S -emit-llvm -O2 -o build/test.bc -c test-0.c
