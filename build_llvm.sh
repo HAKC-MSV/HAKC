dd7e3028eb66ec470e5fae995734e4df8d583fbb
Derrick McKee <derrick.mckee@gmail.com>
2025-01-06 15:00:54 -0500

    fixing incorrect c++ compiler

--- build_llvm.sh
+++ build_llvm.sh
@@ -1,10 +1,9 @@
-#/bin/sh
+#!/bin/bash
 # build llvm and hakc with it 
 source vars.sh
 
-# rm -rf $HAKC_ROOT/cmake-build-hakc-llvm/
-exec_cmd_and_check_status "mkdir -p $HAKC_LLVM_BUILD_PATH"
-exec_cmd_and_check_status "cd $HAKC_LLVM_BUILD_PATH"
+cd $HAKC_LLVM_BUILD_PATH
+echo "Building LLVM in $PWD"
 
 # strange, but note that enable_runtimes is set to '' (don't put compiler-rt here)
 cmake --fresh -G Ninja \
@@ -13,7 +12,7 @@ cmake --fresh -G Ninja \
 -DCMAKE_INSTALL_PREFIX=$HAKC_INSTALL_PATH \
 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 -DCMAKE_C_COMPILER=clang \
--DCMAKE_CXX_COMPILER=clang \
+-DCMAKE_CXX_COMPILER=clang++ \
 -DLLVM_TARGETS_TO_BUILD='X86;AArch64;ARM' \
 -DLLVM_USE_LINKER=lld \
 -DLLVM_ENABLE_IDE=True \
