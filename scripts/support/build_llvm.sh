3e2034784d690004d6a9f7aef61af089e6263290
Derrick McKee <derrick.mckee@ll.mit.edu>
2025-03-20 16:30:43 -0400

    fixing build script

--- scripts/support/build_llvm.sh
+++ scripts/support/build_llvm.sh
@@ -4,17 +4,20 @@ source $(git rev-parse --show-toplevel)/scripts/support/vars.sh
 
 mkdir -p $HAKC_LLVM_BUILD_PATH
 cd $HAKC_LLVM_BUILD_PATH
-CMAKE_INIT_CMD="
-cmake -G Ninja
-      -DCMAKE_INSTALL_PREFIX=$HAKC_INSTALL_PATH
-      -DCMAKE_BUILD_TYPE=RelWithDebInfo
-      $(grep -v '^#' $HAKC_ROOT/scripts/llvm-cmake.sh)
-      -DHAKC_PYTHON_VENV=$HAKC_ROOT/python/venv
-      $HAKC_ROOT
-"
-echo "Building LLVM in $PWD using command "
-echo $CMAKE_INIT_CMD
 
-exec $CMAKE_INIT_CMD
+echo "Building LLVM in $PWD using command "
+cmake -G Ninja \
+  -DCMAKE_INSTALL_PREFIX=$HAKC_INSTALL_PATH \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DHAKC_PYTHON_VENV=$HAKC_ROOT/python/venv \
+  -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;lld' \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DLLVM_TARGETS_TO_BUILD='X86;AArch64;ARM' \
+  -DLLVM_USE_LINKER=lld \
+  -DLLVM_ENABLE_IDE=On \
+  -DLLVM_ENABLE_HAKC=On \
+  -DLLVM_BUILD_TESTS=On \
+  $HAKC_ROOT
 
 cmake --build . -j$(nproc) --target install
