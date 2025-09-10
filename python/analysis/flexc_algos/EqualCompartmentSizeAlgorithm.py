c26b79343227e7890455c8e1ebb4c42f8c92d632
Derrick McKee <derrick.mckee@ll.mit.edu>
2025-09-10 15:08:51 -0400

    correcting data type issues

--- python/analysis/flexc_algos/EqualCompartmentSizeAlgorithm.py
+++ python/analysis/flexc_algos/EqualCompartmentSizeAlgorithm.py
@@ -34,27 +34,38 @@ class EqualCompartmentSizeAlgorithm(FlexCAlgorithm.FlexCAlgorithm):
 
         symbol_hashes = list()
         added_hashes = set()
-        cmd = f"""
+        s0hashes = set()
+        s1hashes = set()
+        remaining_hashes = set()
+        cmd0 = f"""
         MATCH (s0:{HAKCSymbol.get_table_name()})-[e:{HAKCSymbol.relation_dag}]->(s1:{HAKCSymbol.get_table_name()})
-        RETURN s0.{str(HAKCSymbol.get_primary_key())} as SymbolHash0, s1.{str(HAKCSymbol.get_primary_key())} as SymbolHash1, e.weight as Weight
-        ORDER BY Weight
+        RETURN s0.{str(HAKCSymbol.get_primary_key())} as SymbolHash0, 
+        s1.{str(HAKCSymbol.get_primary_key())} as SymbolHash1, 
+        e.weight as Weight
+        ORDER BY Weight DESC, SymbolHash0 DESC
         """
-        data = db.execute_prepared_stmt(cmd).get_as_df()
+        data = db.execute_prepared_stmt(cmd0).get_as_df().astype(
+            {'SymbolHash0': int, 'SymbolHash1': int, 'Weight': int})
         for _, row in data.iterrows():
-            s0hash = row['SymbolHash0']
-            s1hash = row['SymbolHash1']
+            s0hash = int(row['SymbolHash0'])
+            s1hash = int(row['SymbolHash1'])
             if s0hash not in added_hashes:
                 symbol_hashes.append(s0hash)
                 added_hashes.add(s0hash)
+            s0hashes.add(s0hash)
 
             if s1hash not in added_hashes:
                 symbol_hashes.append(s1hash)
                 added_hashes.add(s1hash)
+            s1hashes.add(s1hash)
 
         for symbol_hash in all_symbol_hashes:
+            symbol_hash = int(symbol_hash)
             if symbol_hash not in added_hashes:
                 symbol_hashes.append(symbol_hash)
                 added_hashes.add(symbol_hash)
+                remaining_hashes.add(symbol_hash)
+        del data
 
         logger.info(f'Separating {len(symbol_hashes)} symbols into compartments of size {symbols_per_compartment}')
 
@@ -63,7 +74,7 @@ class EqualCompartmentSizeAlgorithm(FlexCAlgorithm.FlexCAlgorithm):
         for batched_symbols in logger.progress_bar(iterable=itertools.batched(symbol_hashes, symbols_per_compartment),
                                                    desc="Compartmentalizing symbols"):
             for symbol_hash in batched_symbols:
-                compartment_map[symbol_hash] = current_compartment_id
+                compartment_map[int(symbol_hash)] = current_compartment_id
             current_compartment_id += 1
 
         logger.info(f'Deleting compartments')
@@ -75,18 +86,16 @@ class EqualCompartmentSizeAlgorithm(FlexCAlgorithm.FlexCAlgorithm):
         DETACH DELETE d, c;
         """
         db.execute_prepared_stmt(cmd)
-        logger.info(f'Compartmentalization now has {len(db.get_all_compartments())} compartments')
 
         compartment_ids = list(range(1, current_compartment_id))
         division_hashes = [hash(compartment_id) for compartment_id in compartment_ids]
-        access_tokens = [compartment_id << 16 & 1 for compartment_id in compartment_ids]
+        access_tokens = [compartment_id << 16 | 1 for compartment_id in compartment_ids]
         logger.info(f'Inserting {current_compartment_id} new divisions')
         df = pd.DataFrame({
             'division_hash': division_hashes,
             'AccessToken': access_tokens,
             'DivisionID': list(itertools.repeat(1, len(compartment_ids))),
         })
-        logger.info(f'{df}')
         db.insert_from_dataframe(HAKCDivision.get_table_name(), df)
 
         logger.info(f'Inserting compartments')
@@ -103,4 +112,20 @@ class EqualCompartmentSizeAlgorithm(FlexCAlgorithm.FlexCAlgorithm):
         })
         db.insert_from_dataframe(HAKCDivision.relation_compartment, df)
 
+        logger.info(f'Adding symbol division edges')
+        symbols = list()
+        divisions = list()
+        for symbol_hash, compartment_id in compartment_map.items():
+            symbols.append(int(symbol_hash))
+            divisions.append(hash(compartment_id))
+
+        df = pd.DataFrame({
+            'from': symbols,
+            'to': divisions
+        })
+        db.insert_from_dataframe(HAKCSymbol.relation_division, df)
+
         logger.info(f'Compartmentalization now has {len(db.get_all_compartments())} compartments')
+        for compartment_id in [compartment_ids[0], compartment_ids[-1]]:
+            logger.info(
+                f'Compartment {compartment_id} has {len(db.get_all_symbol_hashes_in_compartment(compartment_id))} symbols')
